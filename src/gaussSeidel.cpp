1ffcf48a91fa36832958c91fcd84a87effbb63cb
mehamidi mehamidi <mehamidi@romeologin1.ro18.urca>
2023-03-19 22:07:06 +0100

    rajout de l'algo gaussSeidel

--- src/gaussSeidel.cpp
+++ src/gaussSeidel.cpp
@@ -13,16 +13,18 @@ bool AddGaussSeidel(const Mat m_Src, Mat &m_Dst)
         cout<<"[Error]! Input Image Empty!";
         return 0;
     }
-    // 
     
     // Add a border of zeros to the source matrix
     copyMakeBorder(m_Src,m_Src_border,size_border,size_border,size_border,size_border,border_type,0);
 
-    //
+    // itterat over each pixel of the image
     for(int row = 1; row < m_Src.rows - 1; ++row){
-        // Rajout des indices décalés
+
         for(int col = 1; col < m_Src.cols - 1; ++col){
+
+            // calculate the value of a pixel for each channel following the gaussSeidel method
             for(int chanel = 0; chanel < 3; chanel++){
+                
                 m_Dst.at<Vec3b>(row,col)[chanel] = (m_Dst.at<Vec3b>(row - 1,col)[chanel] +
                 m_Dst.at<Vec3b>(row,col - 1)[chanel] + m_Src_border.at<Vec3b>(row + 1,col)[chanel] +
                 m_Src_border.at<Vec3b>(row,col + 1)[chanel] + m_Src_border.at<Vec3b>(row,col)[chanel]) / 5;
